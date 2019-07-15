05290f92c74b589d94bfb05d11859fc4d398fc85
suji0157 <kim36273@naver.com>
2019-07-15 17:45:57 +0900

    2019.7.15 수업 최종본

--- basic/test.py
+++ basic/test.py
@@ -1,2 +1,15 @@
 print('hi')
 print("hi")
+food="python's favorit food is perl"
+print(food)
+print(food[3])
+print(food[1:5])
+
+a=5
+print('i eat %d apples.'%a)
+
+print('%4s'%'hhhhhh')
+
+print('%0.2f'% 3.1415345654563)
+
+print('python is {1} favorit {0}'.format('zz',12))
