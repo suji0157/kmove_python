a4e45bd80ca4f01e1222c5d15dd688582129f3a4
suji0157 <kim36273@naver.com>
2019-07-16 15:46:08 +0900

    2019.7.16 열공함

---
+++ basic/if_test.py
@@ -0,0 +1,22 @@
+money = True
+if money:
+    print('택시타고 가든지')
+#내어쓰기는 쉬프드+탭
+else:
+    pass #내용 비워둘 때 사용함
+    #print('걸어가든지')
+
+#if조건문 비교연산
+
+x=3
+y=2
+
+print(x>y)
+
+# 만약 3000원 이상의 돈을 가지고 있으면 택시를 타고 그렇지 않으면 걸어 가라.
+
+money1 = 2000
+if money1 >= 3000:
+    print('택시ㄱ')
+else:
+    print('걸어서ㄱ')
--- basic/list_test.py
+++ basic/list_test.py
@@ -1,2 +1,6 @@
-a=[]
-b=[1,2,3]
+a={'name':'pey','phone':'0119993323','birth':'1118'}
+print('name'in a)
+
+print('email'in a)
+
+#이거 주석임 주석 
