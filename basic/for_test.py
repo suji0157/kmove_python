ab95207a6cbd81ddb08b43d3a11e8bb1bb892812
suji0157 <kim36273@naver.com>
2019-07-17 14:21:52 +0900

    2019.7.17

--- basic/for_test.py
+++ basic/for_test.py
@@ -2,5 +2,3 @@ for i in range(2,10):
     for j in range(1,10):
         print('%d * %d = %d' %(i,j,i*j))
     print()
-
-#존나 어렵네
--- basic/rotto.py
+++ basic/rotto.py
@@ -1,7 +1,7 @@
 
 import random
 
-gen_count = 5
+#gen_count = 5
 
 #1번째방법
 
@@ -14,8 +14,20 @@ gen_count = 5
 
 
 #2번째방법
-for x in range(0, gen_count):
-    arr_selected = random.sample(range(1,46), 6)
-    arr_selected.sort()
-    print((' %2d '*6) % tuple(arr_selected))
 
+#for x in range(0, gen_count):
+#    arr_selected = random.sample(range(1,46), 6)
+#    arr_selected.sort()
+#    print((' %2d '*6) % tuple(arr_selected))
+
+
+#3번째방법
+
+for i in range(5):
+    lotto = [0,0,0,0,0,0]
+    for x in range(6):
+        num=0
+        while(num in lotto):
+            num=random.randint(1,46)
+        lotto[x]=num
+    print('lotto :',sorted(lotto))
