3cce1f54747c5059417fb977e4c3199102c3ebef
suji0157 <kim36273@naver.com>
2019-07-17 13:22:35 +0900

    2019.7.17 로또만들기 과제

--- basic/rotto.py
+++ basic/rotto.py
@@ -1,4 +1,21 @@
 
 import random
 
-print(random.randint(1,45))
+gen_count = 5
+
+#1번째방법
+
+#arr = [x for x in range(1,46)]
+#for x in range(0, gen_count):
+#    random.shuffle(arr)
+#    arr_selected = arr[:6]
+#    arr_selected.sort()
+#    print(arr_selected)
+
+
+#2번째방법
+for x in range(0, gen_count):
+    arr_selected = random.sample(range(1,46), 6)
+    arr_selected.sort()
+    print((' %2d '*6) % tuple(arr_selected))
+
